ef746c009bb6ad08d638f34cd908b2574fc8bbfa
brokecarnotauro <brokecarnotauro@gmail.com>
2024-03-21 03:02:03 +0000

    main nav finish

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 const arrowSVG = document.querySelector("#Dribbble-Light-Preview");
 const navDropdown = document.querySelector(".nav-dropdown");
-//arrowSVG.setAttribute("fill","#e60012");
-arrowSVG.setAttribute("fill","#434242");
-
 const searchBar = document.querySelector(".search-bar");
 const navGlass = document.querySelector(".nav-glass");
+const support = document.querySelector("#support-svg-style-stroke");
+const wishlist = document.querySelector("#wishlist-svg-fill");
+const cart = document.querySelector("#cart-svg-fill");
+const login = document.querySelector("#login-svg-fill");
+const navSupport = document.querySelector("#nav-support");
+const navWishlist = document.querySelector("#nav-wishlist");
+const navCart = document.querySelector("#nav-cart");
+const navLogin = document.querySelector("#nav-login");
 
+support.style.stroke = "#434242";
+wishlist.setAttribute("fill", "#434242");
+cart.setAttribute("fill", "#434242");
+login.setAttribute("fill", "#434242");
 navGlass.style.stroke = "#434242";
+arrowSVG.setAttribute("fill","#434242");
+
+const navRightElements = document.querySelectorAll(".nav-right-item");
 
 searchBar.addEventListener("mouseover", () => {
     navGlass.style.stroke = "#e60012";
@@ -22,4 +34,40 @@ navDropdown.addEventListener("mouseover", () => {
 
 navDropdown.addEventListener("mouseout", () => {
     arrowSVG.setAttribute("fill","#434242");
+});
+
+navSupport.addEventListener("mouseover", () => {
+    support.style.stroke = "#e60012";
+    navSupport.style.cursor = "pointer";
+});
+
+navSupport.addEventListener("mouseout", () => {
+    support.style.stroke = "#434242";
+});
+
+navWishlist.addEventListener("mouseover", () => {
+    wishlist.setAttribute("fill", "#e60012");
+    navWishlist.style.cursor = "pointer";
+});
+
+navWishlist.addEventListener("mouseout", () => {
+    wishlist.setAttribute("fill", "#434242");
+});
+
+navCart.addEventListener("mouseover", () => {
+    cart.setAttribute("fill", "#e60012");
+    navCart.style.cursor = "pointer";
+});
+
+navCart.addEventListener("mouseout", () => {
+    cart.setAttribute("fill", "#434242");
+});
+
+navLogin.addEventListener("mouseover", () => {
+    login.setAttribute("fill", "#e60012");
+    navLogin.style.cursor = "pointer";
+});
+
+navLogin.addEventListener("mouseout", () => {
+    login.setAttribute("fill", "#434242");
 });
