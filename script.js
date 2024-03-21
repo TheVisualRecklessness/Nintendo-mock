a42052f1d28f86c8fd1533178e900aab92f192cd
brokecarnotauro <brokecarnotauro@gmail.com>
2024-03-21 02:17:04 +0000

    top left bar finished

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 const arrowSVG = document.querySelector("#Dribbble-Light-Preview");
+const navDropdown = document.querySelector(".nav-dropdown");
 //arrowSVG.setAttribute("fill","#e60012");
 arrowSVG.setAttribute("fill","#434242");
+
+const searchBar = document.querySelector(".search-bar");
+const navGlass = document.querySelector(".nav-glass");
+
+navGlass.style.stroke = "#434242";
+
+searchBar.addEventListener("mouseover", () => {
+    navGlass.style.stroke = "#e60012";
+});
+
+searchBar.addEventListener("mouseout", () => {
+    navGlass.style.stroke = "#434242";
+});
+
+navDropdown.addEventListener("mouseover", () => {
+    arrowSVG.setAttribute("fill","#e60012");
+});
+
+navDropdown.addEventListener("mouseout", () => {
+    arrowSVG.setAttribute("fill","#434242");
+});
